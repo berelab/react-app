41898ad11a72ecdd5d12c5d08dad08805f0c7d67
Berenice Labastida <bere_bere92@hotmail.com>
2024-11-23 18:17:26 +0100

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+"use strict";
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node-modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	plugins: [
+		new HtmlPlugin({
+			template: "./src/index.html",
+		}),
+	],
+	devServer: {
+		static: "./dist",
+		open: true,
+		port: 3000,
+	},
+	mode: "development",
+};
