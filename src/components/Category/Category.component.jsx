b3d5cc3dc1177e3a3e69d11d8dd3e2bbd5bbc2f7
Berenice Labastida <bere_bere92@hotmail.com>
2024-11-24 20:20:10 +0100

    adjustments in movies and categories

--- src/components/Category/Category.component.jsx
+++ src/components/Category/Category.component.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import './Category.scss';
 
 const Category = ({ genre }) => {
@@ -7,7 +8,10 @@ const Category = ({ genre }) => {
       <h2>{title}</h2>
       <div className="category__carousel">
         {movies.map((film) => (
-          <img key={film.id} value={film.id}  className='category__carousel__item' src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt={film.title} />
+          <div key={film.id} className='category__carousel__item'> 
+            <img value={film.id} src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt={film.title} />
+            <p className='category__carousel__title'>{film.title}<br/><span>{film.release_date}</span></p>
+          </div>
         ))}
       </div>
     </div>
--- src/components/Movies/Movies.component.jsx
+++ src/components/Movies/Movies.component.jsx
@@ -1,8 +1,9 @@
 import Category from "../Category/Category.component"
+import './Movies.scss';
 
 const Movies = ({movies}) => {
   return (
-    <div className="movies-container">
+    <div className="movies__container">
       {movies.map((genre) => (
         <Category key={genre.id} genre={genre}/>        
       ))}
