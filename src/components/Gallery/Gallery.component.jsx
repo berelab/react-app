550704117f81e7fd4339b1103e17756608200c67
Berenice Labastida <bere_bere92@hotmail.com>
2024-11-30 15:04:33 +0100

    watchlist context in favorites section

--- src/components/Gallery/Gallery.component.jsx
+++ src/components/Gallery/Gallery.component.jsx
@@ -1,12 +1,15 @@
 import './Gallery.scss';
+import { Link } from 'react-router-dom';
 
 const Gallery = ({ list }) => {
   return (
-    <div className="grid">
-      {list.map(({ title , imageUrl }, index ) => (
-        <div key={index} className='item'>
-          <img className='item__image' src={imageUrl}/>
-          <p className='item__title'>{title}</p>
+    <div className="fav__container">
+      {list.map(({id, title , poster_path, release_date, addedAt}, index ) => (
+        <div key={index} className="gallery">
+          <Link to={`/movie/${id}`} className='gallery__item'>
+            <img className='gallery__item__image' value={id} src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={title} />
+            <p className='gallery__item__title'>{title}<br/><span>{release_date}</span></p>
+          </Link>
         </div>        
       ))}
     </div>
--- src/routes/Favorites/Favorites.jsx
+++ src/routes/Favorites/Favorites.jsx
@@ -1,60 +1,10 @@
 import Gallery from "../../components/Gallery/Gallery.component";
-
+import { useWatchlist } from '../../context/WatchlistContext';
 
 const Favorites = () => {
-  const movieList = [
-    {
-      id: 1,
-      title: 'Amor y otras drogas',
-      imageUrl : 'https://media.themoviedb.org/t/p/w220_and_h330_face/bzXczujSYdsddv9FZBmVhPUN65z.jpg',
-    },
-    {
-      id: 2,
-      title: 'Wicked',
-      imageUrl: 'https://media.themoviedb.org/t/p/w220_and_h330_face/q1czoLwMaiUO1bznWuETCP5ueZj.jpg',
-    },
-    {
-      id: 3,
-      title: 'From',
-      imageUrl : 'https://media.themoviedb.org/t/p/w220_and_h330_face/cjXLrg4R7FRPFafvuQ3SSznQOd9.jpg',
-    },
-    {
-      id: 4,
-      title: 'Amor y otras drogas',
-      imageUrl : 'https://media.themoviedb.org/t/p/w220_and_h330_face/bzXczujSYdsddv9FZBmVhPUN65z.jpg',
-    },
-    {
-      id: 5,
-      title: 'Wicked',
-      imageUrl: 'https://media.themoviedb.org/t/p/w220_and_h330_face/q1czoLwMaiUO1bznWuETCP5ueZj.jpg',
-    },
-    {
-      id: 6,
-      title: 'From',
-      imageUrl : 'https://media.themoviedb.org/t/p/w220_and_h330_face/cjXLrg4R7FRPFafvuQ3SSznQOd9.jpg',
-    },
-    {
-      id: 7,
-      title: 'Amor y otras drogas',
-      imageUrl : 'https://media.themoviedb.org/t/p/w220_and_h330_face/bzXczujSYdsddv9FZBmVhPUN65z.jpg',
-    },
-    {
-      id: 8,
-      title: 'Wicked',
-      imageUrl: 'https://media.themoviedb.org/t/p/w220_and_h330_face/q1czoLwMaiUO1bznWuETCP5ueZj.jpg',
-    },
-    {
-      id: 9,
-      title: 'From',
-      imageUrl : 'https://media.themoviedb.org/t/p/w220_and_h330_face/cjXLrg4R7FRPFafvuQ3SSznQOd9.jpg',
-    },
-    {
-      id: 10,
-      title: 'Wicked',
-      imageUrl: 'https://media.themoviedb.org/t/p/w220_and_h330_face/q1czoLwMaiUO1bznWuETCP5ueZj.jpg',
-    }
-  ]
-  return <Gallery list={movieList}/>;
+  const { watchlist } = useWatchlist();
+
+  return <Gallery list={watchlist}/>;
 };
   
 
