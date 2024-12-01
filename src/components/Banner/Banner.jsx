91666ef757cfa172a4f467eaad466db6a1e6641a
Berenice Labastida <bere_bere92@hotmail.com>
2024-12-01 17:45:42 +0100

    styles refactor

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,15 @@
 import './Banner.scss';
+import { useLocation } from 'react-router-dom';
 
 const Banner = ({details}) => {
+  const location = useLocation();
+  const {genre} = location.state || {genre : 'Unknown'};
   return (
     <div className="banner">
       <div className="banner__container">
         <img alt={details.title} src={`https://image.tmdb.org/t/p/original${details.backdrop_path}`}/>
         <div className='banner__slogan'>
-          <h2>{details.title}<span> ⭐ {details.vote_average}</span></h2>
+          <h2 className={`${genre? genre.trim() : ''}`}>{details.title}<span> ⭐ {details.vote_average}</span></h2>
           <p>{details.tagline}</p>
         </div>        
       </div>
